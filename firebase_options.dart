d976e0f0a36f37a687d7ccf8f5a91c866a52e093
bondhu-app <md.mojidul.haque.1234@gmail.com>
2026-08-16 02:48:17 +0600

    Add files via upload

---
+++ firebase_options.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+
+/// Generated for the Bondhu Social Firebase Android app.
+///
+/// This project currently targets Android. If you later add iOS/web,
+/// run `flutterfire configure` and replace this file with the generated one.
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => android;
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyBiAdWhCS12QN8qhi4esIOjwqQ1zPzetwg',
+    appId: '1:363053068335:android:2edc8d7df9b0c8d8fd16da',
+    messagingSenderId: '363053068335',
+    projectId: 'bondhu-social-c01e2',
+    storageBucket: 'bondhu-social-c01e2.firebasestorage.app',
+  );
+}
