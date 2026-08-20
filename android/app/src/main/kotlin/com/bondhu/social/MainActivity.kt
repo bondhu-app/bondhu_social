69fe0728910374a8219541c3d9df661aa2346be2
bondhu-app <md.mojidul.haque.1234@gmail.com>
2026-08-21 00:51:40 +0600

    Remove Google Mobile Ads plugin configuration
    Removed Google Mobile Ads plugin integration from MainActivity.

--- android/app/src/main/kotlin/com/bondhu/social/MainActivity.kt
+++ android/app/src/main/kotlin/com/bondhu/social/MainActivity.kt
@@ -2,31 +2,10 @@ package com.bondhu.social
 
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
 class MainActivity : FlutterActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-
-        GoogleMobileAdsPlugin.registerNativeAdFactory(
-            flutterEngine,
-            "listTile",
-            ListTileNativeAdFactory(this)
-        )
-    }
-
-    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
-        GoogleMobileAdsPlugin.unregisterNativeAdFactory(
-            flutterEngine,
-            "listTile"
-        )
-
-        super.cleanUpFlutterEngine(flutterEngine)
-    }
-}ine
+}
