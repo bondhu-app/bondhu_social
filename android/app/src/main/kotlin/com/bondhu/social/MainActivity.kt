945df0f4ee1b285e4e1a565a562de048acc2a3d9
bondhu-app <md.mojidul.haque.1234@gmail.com>
2026-08-21 00:43:31 +0600

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/bondhu/social/MainActivity.kt
+++ android/app/src/main/kotlin/com/bondhu/social/MainActivity.kt
@@ -1,41 +1,32 @@
 package com.bondhu.social
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
 class MainActivity : FlutterActivity() {
 
-    private var nativeAdFactory: ListTileNativeAdFactory? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
-    override fun configureFlutterEngine(
-        flutterEngine: FlutterEngine
-    ) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-
-        nativeAdFactory = ListTileNativeAdFactory(
-            this
-        )
 
         GoogleMobileAdsPlugin.registerNativeAdFactory(
             flutterEngine,
             "listTile",
-            nativeAdFactory!!
+            ListTileNativeAdFactory(this)
         )
     }
 
-    override fun cleanUpFlutterEngine(
-        flutterEngine: FlutterEngine
-    ) {
+    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
         GoogleMobileAdsPlugin.unregisterNativeAdFactory(
             flutterEngine,
             "listTile"
         )
 
-        nativeAdFactory = null
-
-        super.cleanUpFlutterEngine(
-            flutterEngine
-        )
+        super.cleanUpFlutterEngine(flutterEngine)
     }
-}
+}ine
