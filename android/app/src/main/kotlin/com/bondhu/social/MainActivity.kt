e81fb37aa2d7963f896bcbe143f96002e3795289
bondhu-app <md.mojidul.haque.1234@gmail.com>
2026-08-21 01:26:41 +0600

    Simplify MainActivity class definition

--- android/app/src/main/kotlin/com/bondhu/social/MainActivity.kt
+++ android/app/src/main/kotlin/com/bondhu/social/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.bondhu.social
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : FlutterActivity()
